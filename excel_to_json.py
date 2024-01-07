2f37a054b9c4466b57e37e37c7f5b2be105b307b
liuly <me@liuly.moe>
2024-01-07 20:39:18 +0800

    add html demo

--- excel_to_json.py
+++ excel_to_json.py
@@ -5,10 +5,19 @@ import json
 for area in areas:
     filename = resultFolder + area.get("name") + "-漫展信息.xlsx"
     df_dicts = pd.read_excel(filename, sheet_name=None)
-    for key in df_dicts.keys():
-        df_dicts[key] = df_dicts[key].to_dict(orient="records")
+    for sheet in df_dicts.keys():
+        df_dicts[sheet] = df_dicts[sheet].to_dict(orient="records")
+        # del 0 key in each sheet dict
+        for item in df_dicts[sheet]:
+            del item[0]
     json.dump(
         df_dicts,
         open(filename.replace(".xlsx", ".json"), "w", encoding="utf-8"),
         ensure_ascii=False,
     )
+
+# save update time in timestamp.txt
+import time
+
+with open(resultFolder + "timestamp.txt", "w", encoding="utf-8") as f:
+    f.write(str(time.time()))
