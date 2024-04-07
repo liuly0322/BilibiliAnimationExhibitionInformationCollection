b2f1b0146b5a98a59c787a210072a7b10c275a0b
liuly <me@liuly.moe>
2024-04-07 20:19:45 +0800

    feat: pwa!

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,5 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 createApp(App).mount('#app')
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Bilibili 漫展信息收集',
+        short_name: 'Bilibili 漫展',
+        description: '爬取 Bilibili 漫展信息，按时间排序',
+        lang: 'zh-CN',
+        theme_color: '#ffffff',
+      }
+    })
   ],
   resolve: {
     alias: {
